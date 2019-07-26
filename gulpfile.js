8e78b6e2ece10da7edca7e48e1d9e467938191bc
Jack Davis <jbad90@gmail.com>
2019-07-26 15:30:54 +0100

    Add build configuration

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp');
+const browsersync = require('browser-sync').create();
+const nunjucksRender = require('gulp-nunjucks-render');
+const webpack = require('webpack-stream');
+const del = require('del');
+const sass = require('gulp-sass');
+
+function clean() {
+  return del(['./dist']);
+}
+
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: './dist/',
+    },
+    port: 3000,
+  });
+  done();
+}
+
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+function javascript() {
+  return gulp
+    .src('src/app/index.js')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browsersync.stream());
+}
+
+function css() {
+  return gulp
+    .src('./src/scss/**/*.scss')
+    .pipe(sass({includePaths: ['node_modules']}))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browsersync.stream());
+}
+
+function nunjucks() {
+  return gulp
+    .src('./src/templates/pages/**/*.html', {base: './src/templates/pages'})
+    .pipe(
+      nunjucksRender({
+        path: ['./src/templates/'],
+      }),
+    )
+    .pipe(gulp.dest('./dist'));
+}
+
+function watch() {
+  gulp.watch('./src/scss/**/*', gulp.series(css));
+  gulp.watch('./src/app/**/*', gulp.series(javascript));
+  gulp.watch(
+    ['./src/templates/**/*'],
+    gulp.series(nunjucks, browserSyncReload),
+  );
+}
+
+exports.watch = gulp.parallel(watch, browserSync);
+exports.default = gulp.series(clean, gulp.parallel(nunjucks, css, javascript));
