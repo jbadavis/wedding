ccc75f015cc300de330c0b7b184ad1ff1d15e742
Jack Davis <jbad90@gmail.com>
2019-09-15 19:52:46 +0100

    Initial animate on scroll behaviour

--- src/app/initialiseListeners.js
+++ src/app/initialiseListeners.js
@@ -1,7 +1,13 @@
 import $ from 'jquery';
 import headerHandler from './handlers/headerHandler';
+import initScrollHandler from './handlers/scrollHandler';
 
 const initialiseListeners = () => {
+  const scrollHandler = initScrollHandler();
+
+  scrollHandler()
+
+  $(window).scroll(scrollHandler);
   $('.mj-header-button-wrapper').click(headerHandler);
 };
 
