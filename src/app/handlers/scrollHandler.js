b41a76d198316f720dda8d58f50c374e8f6cd8f3
Jack Davis <jbad90@gmail.com>
2019-10-11 16:03:52 +0100

    Move scroll down component into partial

--- src/app/handlers/scrollHandler.js
+++ src/app/handlers/scrollHandler.js
@@ -1,6 +1,7 @@
-const isElementInViewport = (el, offset = 0.8) => {
+const isElementInViewport = (el) => {
   const {top} = el.getBoundingClientRect();
   const {innerHeight} = window;
+  const offset = el.getAttribute('data-animation-offset') || 0.8
 
   return top <= innerHeight * offset;
 };
