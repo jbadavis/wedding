f56be1e1252ad68dcaac8ca3ec9c7094fc10688b
Jack Davis <jbad90@gmail.com>
2019-11-11 11:58:17 +0000

    Hide rsvp element if footer in viewport
    When the footer comes into view the RSVP element is hidden. This
    replaces the element fixing functionality which was not working as
    expected on iOS.
    
    A new RSVP link is added to the footer.

--- src/app/handlers/scrollHandler/index.js
+++ src/app/handlers/scrollHandler/index.js
@@ -1,17 +1,17 @@
+import isElementInViewport from './isElementInViewport';
 import isElementOutOfViewport from './isElementOutOfViewport';
 import animateOnScroll from './animateOnScroll';
-import initFixRsvp from './fixRsvp';
 
 const initScrollHandler = () => {
   const elsToAnimate = document.getElementsByClassName('animate');
   const scrollDownEl = document.getElementById('scroll-down');
-
-  const fixRsvp = initFixRsvp();
+  const rsvpEl = document.getElementById('rsvp');
+  const footerEl = document.getElementById('footer');
 
   return () => {
     animateOnScroll(elsToAnimate);
-    // fixRsvp();
 
+    rsvpEl.classList.toggle('hide', isElementInViewport(footerEl));
     scrollDownEl.classList.toggle('hide', isElementOutOfViewport(scrollDownEl));
   };
 };
